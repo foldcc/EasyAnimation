4d4144b40ca55c6becd76dad3d9c3be510e8cf65
Foldcc <1813547935@zhbs.com>
2018-09-11 17:35:28 +0800

    新增控制类型“旋转效果”！支持UI和GameObject

---
+++ Assets/EasyAnimation/Scripts/Moulds/EasyAnimation_Rotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyAnimation {
+    [AddComponentMenu("EasyAnimation/旋转效果")]
+    public class EasyAnimation_Rotate : EasyAnimationTemplateMethod
+    {
+        [Header("旋转增量")]
+        public Vector3 incrementRotate = Vector3.zero;
+
+        Vector3 lastRotation;
+
+        protected override void Easy_Animation_Awake()
+        {
+            lastRotation = initRotation.eulerAngles;
+        }
+
+        protected override void PrimitiveOperation_Start()
+        {
+            ead = new EaseAinmationDrive(1, 0, 1, easetype);
+        }
+
+        protected override bool PrimitiveOperation_UpDate(float time)
+        {
+            transform.localRotation = Quaternion.Euler(incrementRotate * ead.getProgress(time) + lastRotation);
+            return true;
+        }
+
+        public override void Rese()
+        {
+            transform.localRotation = initRotation;
+        }
+    }
+}
+
